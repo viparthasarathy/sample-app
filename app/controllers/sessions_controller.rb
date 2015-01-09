efc33f289412ab420acf3ca84dc8208fb3d6fc98
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-01-09 23:15:34 +0000

    reject invalid logins

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      
+    else
+      flash.now[:danger] = 'Invalid password/email combination.'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+end
