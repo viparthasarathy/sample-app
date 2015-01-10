d1d4b63b06439e930f0bdcd4e3ac4d66b5591a01
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-01-10 22:33:38 +0000

    add authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -25,4 +39,15 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
   
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,6 +38,9 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  def current_user?(user)
+    user == current_user
+  end
 
   
 end
