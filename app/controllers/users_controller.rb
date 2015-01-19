f068e5010ed47f9a7b0bccbd6272c73471cc9e04
Vidul Parthasarathy <viparthasarathy@gmail.com>
2015-01-19 08:01:20 +0000

    add feed and corresponding tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,13 +52,6 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
   
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   def correct_user
     @user = User.find(params[:id])
